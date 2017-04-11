a61aa051dc24bedeb99ee8206da64c5374954a33
Dephanie Ho <dephho88@gmail.com>
2017-04-10 21:42:14 -0700

    Connected to Parse and can create user

---
+++ Nudge/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LoginViewController.swift
+//  Nudge
+//
+//  Created by Dephanie Ho on 4/10/17.
+//  Copyright © 2017 Dephanie Ho. All rights reserved.
+//
+
+import UIKit
+import Parse
+import AFNetworking
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func onLogin(_ sender: Any) {
+        
+    }
+    
+    /* Create a new user */
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        let newUser = PFUser()
+        
+        newUser.username = usernameTextField.text
+        newUser.password = pwTextField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("Created a user")
+                //self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
+    
+}
